30aca9d5c4a8f253459ad664602c952635002de9
Şeyma Altıparmak <102421136+seyma-altiparmak@users.noreply.github.com>
2024-04-12 01:35:42 +0300

    appbar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:film_flutterapp/ui/views/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MovieHell',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Center(),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/ui/views/details.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatefulWidget {
+  const DetailsPage({super.key});
+
+  @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("MOVIEHELL 🎞", style: TextStyle(color: Colors.white)), backgroundColor: Colors.purple,),
+      body: const Center(),
+    );
+  }
+}
